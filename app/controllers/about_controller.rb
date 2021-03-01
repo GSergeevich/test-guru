39137e2cc6e9ad3ce1087050d3e6fba2b5f84816
GSergeevich <>
2021-03-01 11:25:59 +0300

    Исправлены замечания(one more time)

--- app/controllers/about_controller.rb
+++
@@ -1,9 +0,0 @@
-class AboutController < ApplicationController
-  def show
-    if  params[:page]
-      render template: "about/#{params[:page]}"
-    else
-      render template: "about/about"
-    end 
-  end
-end
